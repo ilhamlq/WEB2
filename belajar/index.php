42df61bf159afc3c3f081c2fcf7c42e068debb06
ilhamlq <ilhamlatulqadari@gmail.com>
2017-05-02 11:19:16 +0700

    Belajar Include dan Requre

---
+++ belajar/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Sistem Informasi</h1>
+	<?php 
+	include_once 'tabel.php'
+	 ?>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus enim quo consequatur, quidem inventore perspiciatis beatae. Animi eligendi porro similique odio ratione, atque laboriosam quaerat culpa non excepturi. Possimus, excepturi!</p>
+</body>
+</html>
---
+++ belajar/tabel.php
@@ -0,0 +1,21 @@
+
+	<table border="1px">
+			<tr>
+			<td>
+				no
+		
+			</td>
+			<td>
+				NIM
+			</td>
+
+			<td>
+				Mahasiswa
+			</td>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>H1101151004</td>
+				<td>ILHAM</td>
+			</tr>
+	</table>
